1cc01cd5885ff5120f12750eb3991f0871463ce0
thewookery <tylersamanka@gmail.com>
2026-09-03 23:13:35 -0400

    Build v170: Fix brightness scaling on Pebble Seed Pixels for true 100% full-lumen output and smooth 6-step dimming

--- firmware/open_pixel_poi_firmware_platformio/src/led_strip/NeoPixelStrip.cpp
+++ firmware/open_pixel_poi_firmware_platformio/src/led_strip/NeoPixelStrip.cpp
@@ -6,8 +6,19 @@ void NeoPixelStrip::Show() { strip.Show(); }
 void NeoPixelStrip::SetPixelColor(uint16_t i, RgbColor color) { strip.SetPixelColor(i, color); }
 void NeoPixelStrip::ClearTo(RgbColor color) { strip.ClearTo(color); }
 void NeoPixelStrip::SetBrightness(uint8_t i) { 
+#ifdef PEBBLE_50PX
+    // Micro Pebble Seed Pixels draw only ~12mA max per pixel (not 150mA 5050 power LEDs)
+    // Scale 1-100% brightness directly to 0-255 luminance!
+    if (i <= 1) {
+        strip.SetLuminance(i);
+    } else {
+        uint8_t lum = (uint8_t)((i * 255) / 100);
+        strip.SetLuminance(lum);
+    }
+#else
     strip.SetLuminance(
         CalculateLuminance(i, strip.PixelCount(), OUTPUT_WS2812B_5050_DRAW, OUTPUT_WS2812B_5050_LIMIT)
     ); 
+#endif
 }
 uint8_t NeoPixelStrip::GetLuminance() { return strip.GetLuminance(); }
